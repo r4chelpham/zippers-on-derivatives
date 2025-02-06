0805f53cf7ec22fe72d3e0461b2ff71e996ac89d
r4chelpham <r4chelpham@gmail.com>
2025-02-06 23:09:46 +0000

    Create Token class

---
+++ src/Token.hs
@@ -0,0 +1,19 @@
+module Token where
+
+data Token = T_KEYWORD String
+            | T_OP String
+            | T_STRING String
+            | T_PAREN String
+            | T_SEMI
+            | T_ID String
+            | T_NUM Integer deriving (Show)
+
+token :: ([Char], [Char]) -> Token
+token ("k", s) = T_KEYWORD s
+token ("o", s) = T_OP s
+token ("str", s) = T_STRING s
+token ("p", s) = T_PAREN s
+token ("s", _) = T_SEMI
+token ("i", s) = T_ID s
+token ("n", s) = T_NUM (read s :: Integer)
+token _ = error "token was not valid"
