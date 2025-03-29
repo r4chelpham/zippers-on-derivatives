f8006ca972a30e8484b3ad3d472e151a1a345790
Rachel Pham <r4chelpham@gmail.com>
2025-03-29 12:16:29 +0000

    Move Token and While registers out of Lexer file

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,7 +2,8 @@
 module Lexer where
 import Rexp
 import Val
-import qualified Data.Set as Set
+import WhileRegisters
+import Token
 
 mkeps :: Rexp -> Val
 mkeps ONE = Empty
@@ -111,77 +112,6 @@ lexSimp r (c:cs) =
 
 lexingSimp :: Rexp -> String -> [(String, String)]
 lexingSimp r s = Val.env $ lexSimp r s
-
-keyword :: Rexp
-keyword = "while" <|> "if" <|> "then" <|> "else" <|> "do" <|> "for" <|> 
-          "to" <|> "true" <|> "false" <|> "read" <|> "write" <|> 
-          "skip" <|> "break"
-
-op :: Rexp
-op = "+" <|> "-" <|> "*" <|> "%" <|> "/" <|> "==" <|> "!=" <|> ">" <|> 
-     "<" <|> "<=" <|> ">=" <|> ":=" <|> "&&" <|> "||"
-
-lett :: Rexp
-lett = RANGE $ Set.fromList (['A'..'Z'] ++ ['a'..'z'])
-
-sym :: Rexp
-sym = lett <|> RANGE (Set.fromList ['.', '_', '>', '<', '=', ';', ',', '\\', ':'])
-
-parens :: Rexp
-parens = RANGE $ Set.fromList ['(', ')', '{', '}']
-
-digit :: Rexp
-digit = RANGE $ Set.fromList ['0'..'9']
-
-semi :: Rexp
-semi = toRexp ";"
-
-whitespace :: Rexp
-whitespace = PLUS (" " <|> "\n" <|> "\t" <|> "\r")
-
-identifier :: Rexp
-identifier = lett <~> STAR ("_" <|> lett <|> digit)
-
-numbers :: Rexp
-numbers = "0" <|> (RANGE (Set.fromList ['1'..'9']) <~> STAR digit)
-
-string :: Rexp
-string = "\"" <~> STAR (sym <|> digit <|> parens <|> whitespace <|> "\n") <~> "\""
-
-eol :: Rexp
-eol = "\n" <|> "\r\n"
-
-comment :: Rexp
-comment = "//" <~> STAR (sym <|> parens <|> digit <|> toRexp " ") <~> eol
-
-data Token = T_KEYWORD String
-            | T_OP String
-            | T_STRING String
-            | T_PAREN String
-            | T_SEMI
-            | T_ID String
-            | T_NUM String deriving (Show)
-
-token :: (String, String) -> Token
-token ("k", s) = T_KEYWORD s
-token ("o", s) = T_OP s
-token ("str", s) = T_STRING s
-token ("p", s) = T_PAREN s
-token ("s", _) = T_SEMI
-token ("i", s) = T_ID s
-token ("n", s) = T_NUM s
-
-
-whileRegs :: Rexp
-whileRegs = Rexp.STAR (("k" Rexp.<$> keyword)
-                <|> ("o" Rexp.<$> op)
-                <|> ("str" Rexp.<$> string)
-                <|> ("p" Rexp.<$> parens)
-                <|> ("s" Rexp.<$> semi)
-                <|> ("w" Rexp.<$> whitespace)
-                <|> ("i" Rexp.<$> identifier)
-                <|> ("n" Rexp.<$> numbers)
-                <|> ("c" Rexp.<$> comment))
 
 tokenise :: String -> [Token]
 tokenise s = map token $ filter isNotWhitespace $ lexingSimp whileRegs s
---
+++ src/Token.hs
@@ -0,0 +1,19 @@
+module Token where 
+
+data Token = T_KEYWORD String
+            | T_OP String
+            | T_STRING String
+            | T_PAREN String
+            | T_SEMI
+            | T_ID String
+            | T_NUM String deriving (Show, Eq, Ord)
+
+token :: (String, String) -> Token
+token ("k", s) = T_KEYWORD s
+token ("o", s) = T_OP s
+token ("str", s) = T_STRING s
+token ("p", s) = T_PAREN s
+token ("s", _) = T_SEMI
+token ("i", s) = T_ID s
+token ("n", s) = T_NUM s
+token _ = error "Invalid token"
