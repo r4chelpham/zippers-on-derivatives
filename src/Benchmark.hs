de043fac02bea861ebb7457c5aae79d7c225ef85
Rachel Pham <r4chelpham@gmail.com>
2025-04-01 11:47:32 +0100

    Change benchmarking methods

--- src/Benchmark.hs
+++ src/Benchmark.hs
@@ -2,6 +2,7 @@ module Benchmark where
 
 import Criterion.Main
 import Token
+import Rexp
 
 basePath :: [Char]
 basePath = "src/examples/"
@@ -16,19 +17,36 @@ testFiles =
         , "primes.while"
         ]
 
+strings :: [Int] -> [[Char]]
+strings = map (`replicate` 'a')
+
 loadFiles :: [String] -> IO [String]
 loadFiles = mapM (\f -> readFile (basePath ++ f))
 
-benchmarkIO :: String -> (String -> IO [Token]) -> [String] -> [String] -> IO ()
-benchmarkIO fName f files contents = do
+benchmarkLexIO :: String -> (String -> IO [Token]) -> [String] -> [String] -> IO ()
+benchmarkLexIO fName f files contents = do
   let benchmarks = zipWith (\file content ->
                       bench (fName ++ " " ++ file) $ nfIO (f content)
                     ) files contents
   defaultMain benchmarks
 
-benchmark :: String -> (String -> [Token]) -> [String] -> [String] -> IO ()
-benchmark fName f files contents = do
+benchmarkLex :: String -> (String -> [Token]) -> [String] -> [String] -> IO ()
+benchmarkLex fName f files contents = do
   let benchmarks = zipWith (\file content ->
                       bench (fName ++ ": " ++ file) $ nf f content
                     ) files contents
+  defaultMain benchmarks
+
+benchmarkMatch :: [Char] -> Rexp -> (Rexp -> String -> Bool) -> [Int] -> IO ()
+benchmarkMatch text r f lengths = do
+  let benchmarks = zipWith (\s n ->
+                    bench (text ++ show n) $ nf (f r) s
+                  ) (strings lengths) lengths
+  defaultMain benchmarks
+
+benchmarkMatchNTimes :: [Char] -> (Int -> Rexp) -> (Rexp -> String -> Bool) -> [Int] -> IO ()
+benchmarkMatchNTimes text rexpGen f lengths = do
+  let benchmarks = zipWith (\s n ->
+                      bench (text ++ show n) $ nf (f (rexpGen n)) s
+                    ) (strings lengths) lengths
   defaultMain benchmarks
